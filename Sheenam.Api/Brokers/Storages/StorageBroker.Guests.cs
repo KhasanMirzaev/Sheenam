8cf324fa0143fa39002ac2bdc32f0ffec1317026
khasanmirzaev <mirzayevxasan10@gmail.com>
2022-12-06 14:23:00 +0500

    DATA: Model & Migrate Guests

---
+++ Sheenam.Api/Brokers/Storages/StorageBroker.Guests.cs
@@ -0,0 +1,16 @@
+﻿//=================================================
+//Copyright (c) Coalition of Good-Hearted Engineers
+//Free to Use Comfort and Peace
+//=================================================namespace Sheenam.Api.Brokers.Storages
+
+using Microsoft.EntityFrameworkCore;
+using Sheenam.Api.Models.Foundations.Guests;
+
+namespace Sheenam.Api.Brokers.Storages
+
+{
+    public partial class StorageBroker
+    {
+        public DbSet<Guest> Guests {get; set; }
+    }
+}

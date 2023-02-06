a22c62d92f3af33993f5c695b8a1a3855f40fcef
khasanmirzaev <mirzayevxasan10@gmail.com>
2023-02-06 21:02:42 +0500

    ShouldAddGuestAsync -> FAIL

---
+++ Sheenam.Api.Tests/Foundations/Guests/GuestServiceTests.Logic.Add.cs
@@ -0,0 +1,43 @@
+﻿//=================================================
+//Copyright (c) Coalition of Good-Hearted Engineers
+//Free to Use To Find Comfort and Peace
+//=================================================
+
+
+using FluentAssertions;
+using Moq;
+using Sheenam.Api.Models.Foundations.Guests;
+using Xunit;
+
+namespace Sheenam.Api.Tests.Foundations.Guests
+{
+    public partial class GuestServiceTests
+    {
+        [Fact]
+        public async Task ShouldAddGuestAsync()
+        {
+            //Assert(given)
+            Guest randomGuest = CreateRandomGuest();
+            Guest inputGuest = randomGuest;
+            Guest returningGuest = inputGuest;
+            Guest expectedGuest = returningGuest;
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.InsertGuestAsync(inputGuest))
+                    .ReturnsAsync(returningGuest);
+
+            //Arrange(when)
+            Guest actualGuest = 
+                await this.guestService.AddGuestAsync(inputGuest);
+
+            //Act(then)
+            actualGuest.Should().BeEquivalentTo(expectedGuest);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertGuestAsync(inputGuest),
+                    Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}

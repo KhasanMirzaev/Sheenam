62c60dd6349a188eb5548b82aeafbba2b5404581
khasanmirzaev <mirzayevxasan10@gmail.com>
2023-02-09 14:29:04 +0500

    CODE RUB: Add Logging Broker

--- Sheenam.Api.Tests/Services/Foundations/Guests/GuestServiceTests.Logic.Add.cs
+++ Sheenam.Api.Tests/Services/Foundations/Guests/GuestServiceTests.Logic.Add.cs
@@ -39,6 +39,7 @@ namespace Sheenam.Api.Tests.Unit.Services.Foundations.Guests
                     Times.Once);
 
             this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
         }
     }
 }

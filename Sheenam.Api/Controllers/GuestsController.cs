263dd0ee54f84af0de64d5644740e63d05aab182
khasanmirzaev <mirzayevxasan10@gmail.com>
2023-02-13 16:26:10 +0500

    CODE RUB: Fix Logging Error

---
+++ Sheenam.Api/Controllers/GuestsController.cs
@@ -0,0 +1,30 @@
+﻿//=================================================
+//Copyright (c) Coalition of Good-Hearted Engineers
+//Free to Use To Find Comfort and Peace
+//=================================================
+
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Sheenam.Api.Models.Foundations.Guests;
+using Sheenam.Api.Services.Foundations.Guests;
+
+namespace Sheenam.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class GuestsController : Controller
+    {
+        private readonly IGuestService guestService;
+
+        public GuestsController(IGuestService guestService)
+        {
+            this.guestService = guestService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostGuest(Guest guest)
+        {
+            return Ok(await this.guestService.AddGuestAsync(guest));
+        }
+    }
+}
